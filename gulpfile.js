f0ae975a2cf132b158bc7571bdc04d59814ac84a
jorge paiva <jorge_paiva@hotmail.co.uk>
2017-06-29 21:50:15 +0100

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 gutil = require('gulp-util');
 coffee = require('gulp-coffee');
+concat = require('gulp-concat');
+browserify = require('gulp-browserify');
+
 var coffeesources = ['components/coffee/tagline.coffee'];
+var jssources = ['components/scripts/rclick.js',
+'components/scripts/pixgrid.js',
+'components/scripts/tagline.js',
+'components/scripts/template.js'
+];
 gulp.task('coffee', function(){
 
   gulp.src(coffeesources)
   .pipe(coffee({ bare: true }) .on('error', gutil.log))
   .pipe(gulp.dest('components/scripts'))
+});
+gulp.task('js', function(){
+  gulp.src(jssources)
+  .pipe(concat('scripts.js'))
+  .pipe(browserify())
+  .pipe(gulp.dest('builds/development/js'))
+
 });
